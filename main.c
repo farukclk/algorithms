226e299df8e8b4c84d3d8189af078225cd7eb4da
dos <farukcelikclk@gmail.com>
2023-05-28 11:43:41 -0400

    test code

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-#include "tree.h"
+#include "include/tree.h"
 
 
 int main() {
     
+    printf("%d\n", decimal_to_binary(100, 0));
+
+    int a[7] = {1,-20,11,22,11,23,-2};
+
+    merge_sort(a, 0, 6);
+
+    for(int i = 0;i < 7; i++)
+        printf("%d  ", a[i]);
+
+    printf("\n");
+
 }
+
 
